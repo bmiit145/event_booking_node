e9820baea188148e5ab070cbaa64f7006b3d893c
bmiit145 <21bmiit145@gmail.com>
2024-02-03 20:02:38 +0530

    bug solve

--- controller/eventController.js
+++ controller/eventController.js
@@ -18,9 +18,9 @@ const eventData = [
 const upcommingEventData = [
     {
         id: 123,
-        title: "Velzon Project Discussion with Team",
-        start: new Date(2024, 1, 3, 20, 0), // Adjust month (0-indexed) as needed
-        end: new Date(2024, 1, 4, 16, 0),   // Adjust month (0-indexed) as needed
+        title: "Upcommint Project",
+        start: new Date(2024, 1, 6, 20, 0), // Adjust month (0-indexed) as needed
+        end: new Date(2024, 1, 7, 16, 0),   // Adjust month (0-indexed) as needed
         allDay: false,
         className: "bg-info-subtle",
         location: "Head Office, US",
