fa17acf4624bee60bf2ad6173a585b30f133ecf4
bmiit145 <21bmiit145@gmail.com>
2024-02-03 17:36:37 +0530

    Static res set

--- controller/eventController.js
+++ controller/eventController.js
@@ -1,10 +1,26 @@
 
+const eventData = [
+    {
+        id: 456,
+        title: "Velzon Project Discussion with Team",
+        start: new Date(2024, 1, 3, 20, 0), // Adjust month (0-indexed) as needed
+        end: new Date(2024, 1, 4, 16, 0),   // Adjust month (0-indexed) as needed
+        allDay: false,
+        className: "bg-info-subtle",
+        location: "Head Office, US",
+        extendedProps: {
+            department: "Discussion",
+        },
+        description: "Tell how to boost website traffic",
+    },
+];
+
 module.exports = {
     getAllEvent: (req, res) => {
-        res.send("Get all event");
+        res.json(eventData);
     },
 
     getUpcommingEvent: (req, res) => {
-        res.send("Get upcomming event");
+        res.json(eventData);
     }
 }
