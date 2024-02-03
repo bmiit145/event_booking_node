0e33163fbe87c57e2b0cbbff665178edc5551188
bmiit145 <21bmiit145@gmail.com>
2024-02-03 19:12:51 +0530

    cors solve

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 
 
-// app.use(express.json());
+app.use(express.json());
 
 // view engine
 // const hbs = require('hbs')
--- controller/eventController.js
+++ controller/eventController.js
@@ -15,20 +15,28 @@ const eventData = [
     },
 ];
 
+const upcommingEventData = [
+    {
+        id: 123,
+        title: "Velzon Project Discussion with Team",
+        start: new Date(2024, 1, 3, 20, 0), // Adjust month (0-indexed) as needed
+        end: new Date(2024, 1, 4, 16, 0),   // Adjust month (0-indexed) as needed
+        allDay: false,
+        className: "bg-info-subtle",
+        location: "Head Office, US",
+        extendedProps: {
+            department: "Discussion",
+        },
+        description: "Tell how to boost website traffic",
+    },
+];
+
 module.exports = {
     getAllEvent: (req, res) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        res.header('Referrer-Policy', 'no-referrer'); // or 'same-origin', 'origin', etc.
         res.json(eventData);
     },
 
     getUpcommingEvent: (req, res) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        res.header('Referrer-Policy', 'no-referrer'); // or 'same-origin', 'origin', etc.
-        res.json(eventData);
+        res.json(upcommingEventData);
     }
 }
