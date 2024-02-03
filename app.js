a84d4abe187e944b62ebc5b8907156adc2ef3d08
bmiit145 <21bmiit145@gmail.com>
2024-02-03 17:41:35 +0530

    origin set

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express();
 const port = 4001
 
+// Allow requests from any origin
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+// app.use(express.json());
+
 // view engine
 // const hbs = require('hbs')
 // const bodyParser = require('body-parser')
