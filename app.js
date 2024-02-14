66a1ba9dc4e116648d93e4fe2268a2d414ae642c
bmiit145 <21bmiit145@gmail.com>
2024-02-14 13:16:58 +0530

    change og creation of users

--- app.js
+++ app.js
@@ -24,74 +24,80 @@ app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://21bmiit145:root@satnam-decor.kya4kd2.mongodb.net/satnam_decor?retryWrites=true&w=majority" ,{
-    family : 4,
+mongoose.connect("mongodb+srv://21bmiit145:root@satnam-decor.kya4kd2.mongodb.net/satnam_decor?retryWrites=true&w=majority", {
+    family: 4,
 })
-.then(() => {
-       console.log("connected to db");
-}).catch((err) => {
-    console.log("error" + err);
-})
-
-
-// default users
-const userData = [{
-    id: 1,
-    first_name: "Satnam",
-    last_name: "Decor",
-    email: "admin@satnam.com",
-    password: bcrypt.hashSync("123456", 12)
-},
-{
-    id: 2,
-    first_name: "Satnam",
-    last_name: "Decor",
-    email: "kuldeep@satnam.com",
-    password: bcrypt.hashSync("123456", 12)
-
-},
-{
-    id: 3,
-    first_name: "Satnam",
-    last_name: "Decor",
-    email: "ankush@satnam.com",
-    password: bcrypt.hashSync("123456", 12)
-
-},
-{
-    id: 4,
-    first_name: "Satnam",
-    last_name: "Decor",
-    email: "ajay@satnam.com",
-    password: bcrypt.hashSync("123456", 12)
-
-}];
-
-const savePromises = userData.map(async (u) => {
-    // Check if the user already exists by email or id
-    const existingUser = await User.findOne({ $or: [{ id: u.id }, { email: u.email }] });
-
-    if (!existingUser) {
-        // User does not exist, save it
-        await new User(u).save();
-         console.log(`User with email ${u.email} added.`);
-    } else {
-         console.log(`User with email ${u.email} already exists, skipping.`);
-    }
-});
-
-Promise.all(savePromises)
     .then(() => {
-        console.log('All users have been processed.');
+        console.log("connected to db");
+        creationOfUser();
+    }).catch((err) => {
+        console.log("error  ::: " + err);
     })
-    .catch(error => {
-        console.error('Error processing users:', error);
+
+const creationOfUser = async () => {
+    // default users
+    const userData = [{
+        id: 1,
+        first_name: "Satnam",
+        last_name: "Decor",
+        email: "admin@satnam.com",
+        password: bcrypt.hashSync("123456", 12)
+    },
+    {
+        id: 2,
+        first_name: "Satnam",
+        last_name: "Decor",
+        email: "kuldeep@satnam.com",
+        password: bcrypt.hashSync("123456", 12)
+
+    },
+    {
+        id: 3,
+        first_name: "Satnam",
+        last_name: "Decor",
+        email: "ankush@satnam.com",
+        password: bcrypt.hashSync("123456", 12)
+
+    },
+    {
+        id: 4,
+        first_name: "Satnam",
+        last_name: "Decor",
+        email: "ajay@satnam.com",
+        password: bcrypt.hashSync("123456", 12)
+
+    }];
+
+    const savePromises = userData.map(async (u) => {
+        try {
+            // Attempt to find the user by email
+            const existingUser = await User.findOne({ $or: [{ id: u.id }, { email: u.email }] });
+
+            if (!existingUser) {
+                // User does not exist, save it
+                await new User(u).save();
+                console.log(`User with email ${u.email} added.`);
+            } else {
+                console.log(`User with email ${u.email} already exists, skipping.`);
+            }
+        } catch (error) {
+            // Handle errors
+            console.error(`Error processing user ${u.email}: ${error.message}`);
+        }
     });
 
+    Promise.all(savePromises)
+        .then(() => {
+            console.log('All users have been processed.');
+        })
+        .catch(error => {
+            console.error('Error processing users:', error);
+        });
+}
 
 const routes = require("./router");
 const { redirect } = require('express/lib/response');
-app.use("/" , routes);
+app.use("/", routes);
 app.get('/', (req, res) => {
     // res.redirect('/contact')
     res.send('Hello World!')
